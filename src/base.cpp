e2951fb9db791f22383a2ef12209ccdfd5de7572
Rui Azevedo <ruihfazevedo@gmail.com>
2020-05-07 18:20:27 +0000

    code split

---
+++ src/base.cpp
@@ -0,0 +1,3 @@
+#include "base.h"
+
+U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE);
---
+++ src/xfm2.h
@@ -0,0 +1,11 @@
+/* -*- C++ -*- */
+#pragma once
+
+//XFM2 parameter setting - From Rene's code
+
+void set_parameter( int param, int value );
+void set_unit( int unit );
+// Code for setting the parameter on the XFM2
+void set_parameter( int param, int value );
+// Code for getting a selected parameter on the XFM2
+void get_parameter( int param, int value );
